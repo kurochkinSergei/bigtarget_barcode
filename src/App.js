db027f32ee22e7fce644bb08a6ba7a5b6fc4cafc
Sergei Kurochkin <kurochkinsergei95@gmail.com>
2020-06-26 14:02:30 +0300

    fix logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">Hello hack!</div>;
 }
 
 export default App;
