77be02c07dd78943eea284603a9e1f61f2cb8cfa
Sergei Kurochkin <kurochkinsergei95@gmail.com>
2020-06-26 14:02:59 +0300

    Updates

---
+++ precache-manifest.eb2cb886c62b8456b7d79af136847e08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96afb36e72ba530c052ca18654d84092",
+    "url": "/bigtarget_barcode/index.html"
+  },
+  {
+    "revision": "f92a9c3f7115c43541bf",
+    "url": "/bigtarget_barcode/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "499152bf061c4658ec79",
+    "url": "/bigtarget_barcode/static/js/2.7505c78f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bigtarget_barcode/static/js/2.7505c78f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f92a9c3f7115c43541bf",
+    "url": "/bigtarget_barcode/static/js/main.3b58dda6.chunk.js"
+  },
+  {
+    "revision": "d4b3fb01bbf255cedf4b",
+    "url": "/bigtarget_barcode/static/js/runtime-main.1bdca012.js"
+  }
+]);
