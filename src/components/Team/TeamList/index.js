2608f7bdcce6ec40202c3b8220ad2a83df1d8a0f
Sergei Kurochkin <kurochkinsergei95@gmail.com>
2020-06-28 14:21:37 +0300

    add roles

--- src/components/Team/TeamList/index.js
+++ src/components/Team/TeamList/index.js
@@ -9,8 +9,15 @@ export const roles = {
   pitch: "Питч",
   fe: "Frontend",
   rs: "Research",
+  ml: "ML",
+  geo: "Geo",
+  bank: "Bank Scoring",
+  math: "Math",
+  recsys: "RecSys",
   presentation: "Презентация",
   management: "Управление командой",
+  vis: "Data Visualization",
+  react: "React",
 };
 
 export const members = [
@@ -18,7 +25,7 @@ export const members = [
     id: "maks",
     img: maxImg,
     name: "Максим Шевченко",
-    roles: ["ds", "pitch", "presentation"],
+    roles: ["ds", "pitch", "presentation", "ml", "geo", "dev"],
     github: "https://github.com/maks-sh",
     linkedin: "https://www.linkedin.com/in/maks-sh/",
     description: `Power Uplift'ер. Соавтор цикла статей на хабре «Туториал по Uplift моделированию». Разработчик библиотеки scikit-uplift для моделирования Uplift.`,
@@ -27,17 +34,17 @@ export const members = [
     id: "irina",
     img: irinaImg,
     name: "Ирина Елисова",
-    roles: ["ds", "presentation", "management"],
+    roles: ["ds", "presentation", "management", "recsys", "dev", "ml"],
     github: "https://github.com/ElisovaIra",
     linkedin: "https://www.linkedin.com/in/irina-elisova-23b4b4bb/",
     description:
-      "Предприниматель. Соавтор цикла статей на хабре «Туториал по Uplift моделированию». Разработчик библиотеки scikit-uplift для моделирования Uplift.",
+      "Стартапер. Соавтор цикла статей на хабре «Туториал по Uplift моделированию». Разработчик библиотеки scikit-uplift для моделирования Uplift.",
   },
   {
     id: "sergei",
     img: sergeiImg,
     name: "Сергей Курочкин",
-    roles: ["dev", "fe"],
+    roles: ["dev", "fe", "vis", "react"],
     github: "https://github.com/kurochkinSergei",
     description:
       "Javascript разработчик. Ниндзя React приложений с опытом разработки коммерческих решений в сфере bigdata.",
@@ -47,7 +54,7 @@ export const members = [
     img: kirillImg,
     name: "Кирилл Ликсаков",
     linkedin: "https://www.linkedin.com/in/kirill-liksakov-19769a107/",
-    roles: ["ds", "rs"],
+    roles: ["ds", "rs", "ml", "bank", "math"],
     github: "https://github.com/kirrlix1994",
     description:
       "Математик. Танцор. Призер соревнования по Uplift моделированию от Х5. Разработчик библиотеки scikit-uplift для моделирования Uplift.",
